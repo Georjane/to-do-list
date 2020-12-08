1fcc9054cf671d893add953eb7acdda38e2943b9
vmwhoami <vmwhoami@gmail.com>
2020-12-08 21:10:50 +0200

    Add edit button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 import './assets/styles/style.scss';
-
-import Bin from './assets/img/bin.svg';
-
-function component() {
-  const element = document.createElement('svg');
-  element.path = Bin
-
-
-  return element;
-}
-
-document.body.appendChild(component());
