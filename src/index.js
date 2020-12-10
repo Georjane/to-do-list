5be58039abe4c755e86e6f051526a73e7403bec8
vmwhoami <vmwhoami@gmail.com>
2020-12-11 00:57:29 +0200

    Add display and hide functionality for category butto

--- src/index.js
+++ src/index.js
@@ -5,12 +5,24 @@ import { hash } from './components/addCategory';
 
 main();
 
+
+
+// Add cattegory input display
 let btn = document.querySelector('.showCatBtn')
-
-
-
 btn.addEventListener('click', () => {
   let cat = document.querySelector('.category')
   cat.classList.toggle('display')
+  function removeCurrent(curr) {
+    let current = document.querySelector(`${curr}`).cloneNode(true)
+    let clone = current.cloneNode(true);
+    clone.style.fill = "white";
+    btn.innerHTML = ""
+    btn.appendChild(clone);
+  }
+  if (cat.classList.contains("display")) {
+    removeCurrent(`.icon-minus`)
+  } else {
+    removeCurrent(`.icon-plus`)
+  }
 });
 
